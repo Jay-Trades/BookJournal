ae04e9441420fdf186065d6743da0f427425a60e
Jay <jiananchenn@gmail.com>
2020-12-10 14:17:24 -0500

    changed error in variable which was put as str

--- backend.py
+++ backend.py
@@ -3,7 +3,7 @@ import sqlite3
 class Database:
 
     def __init__(self, db):
-        self.conn=sqlite3.connect("db")
+        self.conn=sqlite3.connect(db)
         self.cur=self.conn.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
         self.conn.commit()
